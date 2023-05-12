6b66ae2b08c2b52e6bc4d1bcfc10030717062577
Sourcery AI <>
2023-05-12 06:25:36 +0000

    'Refactored by Sourcery'

--- app/dialog/about/patreon.py
+++ app/dialog/about/patreon.py
@@ -10,23 +10,28 @@ while True:
     member_data_decoded = json.loads(member_data.text)
 
     for member in member_data_decoded["data"]:
-        if len(member["relationships"]["currently_entitled_tiers"]["data"]) > 0:
-            if member["relationships"]["currently_entitled_tiers"]["data"][0]["id"] == "3952333":
-                if len(name_list) > 0:
-                    name_list += ',\n'
-                name = member["attributes"]["full_name"]
-                name_list += "  QStringLiteral(\""
-                name_list += name.translate(str.maketrans({
-                        "\"": "\\\"",
-                        "\\": "\\\\"
-                    }))
-                name_list += "\")"
+        if (
+            len(member["relationships"]["currently_entitled_tiers"]["data"])
+            > 0
+            and member["relationships"]["currently_entitled_tiers"]["data"][0][
+                "id"
+            ]
+            == "3952333"
+        ):
+            if len(name_list) > 0:
+                name_list += ',\n'
+            name = member["attributes"]["full_name"]
+            name_list += "  QStringLiteral(\""
+            name_list += name.translate(str.maketrans({
+                    "\"": "\\\"",
+                    "\\": "\\\\"
+                }))
+            name_list += "\")"
 
     if "links" in member_data_decoded:
        url = member_data_decoded["links"]["next"]
     else:
        break
 
-text_file = open("patreon.h", "w", encoding="utf-8")
-text_file.write("#ifndef PATREON_H\n#define PATREON_H\n\n#include <QStringList>\n\nQStringList patrons = {\n%s\n};\n\n#endif // PATREON_H\n" % name_list)
-text_file.close()
+with open("patreon.h", "w", encoding="utf-8") as text_file:
+    text_file.write("#ifndef PATREON_H\n#define PATREON_H\n\n#include <QStringList>\n\nQStringList patrons = {\n%s\n};\n\n#endif // PATREON_H\n" % name_list)
